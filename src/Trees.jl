243f74bd9b4f5e60172d6e4ca0f6253efc51305d
Erik Engheim <erik.engheim@mac.com>
2017-03-16 19:10:00 +0100

    Simple red-black tree test data
    When doing a left rotate of the left rotatable tree we whould get the right rotable tree and visa versa.

--- src/Trees.jl
+++ src/Trees.jl
@@ -1,5 +1,16 @@
 module Trees
 
+import Base: map
+
+# Helper for dealing with Nullable
+function map(f::Function, n::Nullable)
+    if isnull(n)
+        n
+    else 
+        Nullable(f(get(n)))
+    end
+end
+
 include("treenode.jl")
 include("tree.jl")
 
@@ -8,5 +19,23 @@ function test_tree()
   numbers = map(Int, letters)
   Tree(zip(letters, numbers) |> collect |> shuffle)
 end
+
+
+# Tree rotation test data
+#    
+#      a               b
+#     / \      -->    / \
+#    t1  b           a   t3
+#       / \         / \
+#      t2  t3      t1  t2
+
+function left_rotable_tree()
+    Tree(10=>"a", 5=>"t1", 15=>"b", 20=>"t3", 12=>"t2")
+end
+
+function right_rotable_tree()
+    Tree(15=>"b", 10=>"a", 20=>"t3", 5=>"t1", 12=>"t2")
+end
+
 
 end # module
